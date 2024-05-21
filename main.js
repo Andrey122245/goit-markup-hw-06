a50dd8c56f758daca86db2a2b2ae13ab5b0359c5
Andrey122245 <andriysok31@gmail.com>
2024-05-21 17:26:48 +0300

    project added

---
+++ main.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const footerContainer = document.querySelector('.modal-overlay');
+//     var btn =  document.querySelector('.close-black');
+//     btn.addEventListener('click',function(e){
+//         e.preventDefault();
+//         footerContainer.classList.toggle('is-open');
+//         // if (btn.body.classList.contains('is-open')) {
+//             setTimeout(function() {
+//                 footerContainer.classList.add('is-open');
+//             }, 5000);
+//         // }
+//     });
+// }); 
